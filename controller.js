bc75b8d16527e452fbfc8e0e2197eda9733cea3f
jose <jdellanza@gmail.com>
2018-10-22 03:12:39 +0200

    query finished

--- controller.js
+++ controller.js
@@ -5,7 +5,7 @@ const query = (req, res) => {
     testsql.init();
 
     
-    testsql.query('SELECT r.name AS recipe, i.name FROM recipe_ingredient AS t1 INNER JOIN recipe AS r ON t1.recipe_id = r.id INNER JOIN ingredient AS i ON t1.ingredient_id = i.id')
+    testsql.query('SELECT * FROM ( SELECT r.name AS recipe, i.name FROM recipe_ingredient AS t1 INNER JOIN recipe AS r ON t1.recipe_id = r.id INNER JOIN ingredient AS i ON t1.ingredient_id = i.id) AS b1 WHERE recipe LIKE "%'+req.query.name+'%"')
     .then((data) => {
       console.log(data)
         res.writeContinue(data);
@@ -16,9 +16,9 @@ const query = (req, res) => {
         res.end();
     });
 
-    // testsql.query('SELECT * FROM recipe WHERE name LIKE "%'+req.query.name+'%"')
+    // testsql.query('SELECT * FROM recipe_ingredient i WHERE recipe_id LIKE "%'+req.query.name+'%"')
     // .then((data) => {
-    //   console.log(data[0].id)
+    //   console.log(data)
     //     res.writeContinue(data);
     //     res.statusCode = 200;
     //     res.setHeader('Content-Type', 'application/json');
